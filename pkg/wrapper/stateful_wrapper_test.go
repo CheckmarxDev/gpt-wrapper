e89a6b8e6d0250b8f75dd7325d908defc60a8555
BarakH <barak.harari@checkmarx.com>
2024-04-11 13:07:49 +0300

    wip cleaning

--- pkg/wrapper/stateful_wrapper_test.go
+++ pkg/wrapper/stateful_wrapper_test.go
@@ -12,7 +12,10 @@ import (
 
 func TestCallGPT_FS(t *testing.T) {
 	var history []message.Message
-	wrapper := NewStatefulWrapper(connector.NewFileSystemConnector(""), "", apikey, models.GPT3Dot5Turbo, 4, 0)
+	wrapper, err := NewStatefulWrapper(connector.NewFileSystemConnector(""), "", apikey, models.GPT3Dot5Turbo, 4, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
 	id := wrapper.GenerateId()
 	t.Log(id)
 	for _, q := range userQuestions {
@@ -50,7 +53,10 @@ func TestCallGPT_FS(t *testing.T) {
 }
 
 func TestMaskSecrets(t *testing.T) {
-	wrapper := NewStatefulWrapper(connector.NewFileSystemConnector(""), "", apikey, models.GPT3Dot5Turbo, 4, 0)
+	wrapper, err := NewStatefulWrapper(connector.NewFileSystemConnector(""), "", apikey, models.GPT3Dot5Turbo, 4, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
 	id := wrapper.GenerateId()
 	t.Log(id)
 	entries, err := wrapper.MaskSecrets("password=exposed")
